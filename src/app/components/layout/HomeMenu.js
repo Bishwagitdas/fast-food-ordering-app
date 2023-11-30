2562b8e1497cc618f223af1bd0b78a98715d9654
Bishwagit Das <66742918+Bishwagitdas@users.noreply.github.com>
2023-12-01 01:00:00 +0600

    home component done

---
+++ src/app/components/layout/HomeMenu.js
@@ -0,0 +1,19 @@
+import Image from "next/image";
+export default function HomeMenu() {
+    return (
+        <>
+        <section className="">
+        <div>
+        <Image src={'/sallad1.png'} layout={'fill'} objectFit={'contain'} alt={'sallad'} />
+        </div>
+        <div className="text-center">
+        <h3 className="uppercase text-gray-500 font-semibold leading-4">
+        Check out
+        </h3>
+        <h2 className="text-primary font-bold text-4xl italic">Menu</h2>
+        </div>
+        </section>
+       
+        </>
+    );
+}
---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import Header from "./components/layout/Header.js";
+import Hero from "./components/layout/Hero.js";
+import HomeMenu from "./components/layout/HomeMenu.js";
+export default function Home() {
+  return (
+    <>
+     <Header />
+     <Hero />
+     <HomeMenu />
+    
+    </>
+  )
+}
